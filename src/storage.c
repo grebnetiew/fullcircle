3411d9b70b4e90783fc90c9a77e5e4376fad5516
grebnetiew <eweitenberg@gmail.com>
2015-12-15 22:59:07 +0100

    better default colors for aplite

--- src/storage.c
+++ src/storage.c
@@ -2,7 +2,7 @@
 
 void load_palette(Palette *p) {
   *p = (Palette) {
-      COLOR_FALLBACK(persist_exists(KEY_COL_APPOINTMENT) ? persist_read_gcolor(KEY_COL_APPOINTMENT) : GColorWhite,      GColorBlack),
+      COLOR_FALLBACK(persist_exists(KEY_COL_APPOINTMENT) ? persist_read_gcolor(KEY_COL_APPOINTMENT) : GColorBlack,      GColorBlack),
       COLOR_FALLBACK(persist_exists(KEY_COL_MINUTE)      ? persist_read_gcolor(KEY_COL_MINUTE)      : GColorWhite,      GColorWhite),
       COLOR_FALLBACK(persist_exists(KEY_COL_HOUR)        ? persist_read_gcolor(KEY_COL_HOUR)        : GColorWindsorTan, GColorWhite),
       COLOR_FALLBACK(persist_exists(KEY_COL_BACKGROUND)  ? persist_read_gcolor(KEY_COL_BACKGROUND)  : GColorBlack,      GColorBlack),
